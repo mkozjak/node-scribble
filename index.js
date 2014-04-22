c4e0fef1ae424fc6f7c875bbc9ea5c23ff1763cf
Mario Kozjak <kozjakm1@gmail.com>
2014-04-22 09:18:13 +0200

    Use forever.monitor
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- index.js
+++ index.js
@@ -1,63 +1,12 @@
-#!/usr/bin/env node
+var forever = require('forever-monitor');
 
-var exec = require('child_process').exec,
-  os = require('os'),
-  scribble = require('scribble'),
-  child, lastArtist, lastTrack,
-  username = require('./config').username,
-  password = require('./config').password,
-  apiKey = require('./config').apiKey,
-  apiSecret = require('./config').apiSecret;
+var scribble = new (forever.Monitor)('./lib/node-scribble.js', {
+  max: 3,
+  silent: true
+});
 
-var Scrobbler = new scribble(apiKey, apiSecret, username, password);
-var systemType = os.type();
+scribble.on('exit', function() {
+  console.log('exited after three starts');
+});
 
-// method definitions
-function getCurrentState(callback) {
-  child = exec('osascript -e \'tell application "iTunes" to '
-    + '{artist, name, album, duration} of current track '
-    + '& player position & player state\'',
-    function(error, stdout, stderr) {
-      this.info = stdout.substring(0, stdout.length - 1);
-      this.infoArray = info.split(', ');
-
-      this.song = {
-        artist: infoArray[0],
-        track: infoArray[1],
-        album: infoArray[2],
-        position: +infoArray[4] / +infoArray[3],
-        state: infoArray[5]
-      };
-
-      callback(this.song);
-    });
-};
-
-
-// program
-setInterval(function() {
-  getCurrentState(function(song){
-    /*
-    console.log('artist:', song.artist);
-    console.log('title:', song.track);
-    console.log('album:', song.album);
-    console.log('progress:', song.position);
-    console.log('state:', song.state);
-    */
-  
-    if (song.position > 0.9 &&
-      song.artist !== lastArtist &&
-      song.track !== lastTrack &&
-      song.state === 'playing'){
-
-      console.log('upisujem!');
-      lastArtist = song.artist;
-      lastTrack = song.track;
-
-      Scrobbler.Scrobble(song, function(post_return_data) {});
-    }
-    else if (song.position < 0.9 && song.state === 'playing') {
-      Scrobbler.NowPlaying(song, function(post_return_data) {});
-    }
-  });
-}, 2000);
+scribble.start();
